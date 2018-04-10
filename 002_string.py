2aba1617bdf4cd2693728761639924a1e4dd2f2d
ahribori <ahribori@gmail.com>
2018-04-11 00:56:44 +0900

    문자열 사용해보기

---
+++ 002_string.py
@@ -0,0 +1,18 @@
+message = 'hello, world'
+
+print(message)
+print(type(message))
+print(message[0:5])
+print(message[7:12])
+
+print(message.split(','))
+print(message.split(',')[0])
+
+daum = 'daum'
+kakao = 'kakao'
+
+print(daum + kakao)
+print(daum + ' ' + kakao)
+
+
+
